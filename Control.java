dfb8e6a5856e1fd80f7eb1c05afd6e6ac9d71323
John McCain <johnm.freestate@gmail.com>
2016-02-08 09:44:39 -0600

    Add buttonPanel to hold UI Buttons

--- Control.java
+++ Control.java
@@ -52,20 +52,26 @@ public class Control extends JFrame implements ActionListener
 
 		/******INTERFACE CODE******/
 
+		//buttonPanel will hold all of the buttons
+		//the GridLayout will arange the buttons in a 2 row grid, 0 means it can have any number of columns
+		JPanel buttonPanel = new JPanel( new GridLayout(2, 0) );
+		buttonPanel.setPreferredSize( new Dimension(1500, 150) );
+
 		//Create the buttons
+		//The string "Test1" or "Test2" will be the text displayed on the button and the string used to identify the button in the actionPerformed function
 	    JButton buttonTest1 = new JButton("Test1"); 
 	    JButton buttonTest2 = new JButton("Test2");
 	    
-	    //Action Listener is an outside class that lets us know when a button is pressed
-	    //This is why we have to implement the "Action Listener"
-	    //It requires us to have an ActionPerformed(ActionEvent) method
-	    //Action Listener calls this when something happens, so it needs to know that we have it so it doesn't trigger an exception
+	    //Add actionListeners for the buttons
 	    buttonTest1.addActionListener(this);
 	    buttonTest2.addActionListener(this);
 	    
-	    //Add the buttons to our JFrame
-	    add(buttonTest1);
-	    add(buttonTest2);
+	    //Add the buttons to our buttonPanel
+	    buttonPanel.add(buttonTest1);
+	    buttonPanel.add(buttonTest2);
+
+	    //Add the buttonPanel to our JFrame
+	    this.getContentPane().add(buttonPanel);
 
 	    //Reveal ourselves to the world
 		setVisible(true);
@@ -83,6 +89,12 @@ public class Control extends JFrame implements ActionListener
 	{
 		switch(event.getActionCommand())
 		{
+			case "Test1":
+				displayMessage("You pressed Test1");
+				break;
+			case "Test2":
+				displayMessage("You pressed Test2");
+				break;
 			default:
 				displayMessage("ERROR: Unrecognized event");
 				break;
